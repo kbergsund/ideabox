34df7afe65ac1a5fa5e150dc29f16fc6a5f47ef3
Colgan Meanor <colganmeanor7@gmail.com>
2021-09-16 23:46:14 -0500

    Almost complete iteration 2

--- main.js
+++ main.js
@@ -10,26 +10,45 @@ var cardSection = document.querySelector('#js-card-section')
 
 // Event Listeners
 saveButton.addEventListener('click', saveToCard);
+titleInput.addEventListener('input', enableSaveButton);
+bodyInput.addEventListener('input', enableSaveButton);
 
-// function render(title, body) {
-// currentIdea = new Idea({ title: title, body: body })
-// // console.log(currentIdea);
-// ideaCards.push(currentIdea);
-// }
+
+function render(title, body) {
+var newIdea = new Idea({ title: title, body: body })
+ideaCards.push(newIdea);
+}
 
 function saveToCard() {
-    // console.log('hey')
-    cardSection.innerHTML =
+    render(titleInput.value, bodyInput.value)
+    cardSection.innerHTML +=
     `<article class = 'idea-card'>
     <div class = "card-top">
       <img src="./assets/star.svg">
       <img src="./assets/delete.svg">
     </div>
-      <h4>Basic Card Title lol</h4>
-      <p>Idea body. Don't ever play yourself. Every chance I get, I water the plants, Lion! Cloth talk. </p>
+      <h4>${titleInput.value}</h4>
+      <p>${bodyInput.value}</p>
     <div class = "card-bottom">
       <img src="./assets/comment.svg" class = "comment-image">
       <p>Comment</p>
     </div>
   </article>`
+  console.log(ideaCards)
+  clearInputs();
+  disableSaveButton();
+}
+
+function enableSaveButton(){
+    saveButton.disabled = false;
+    saveButton.classList.add('enabled-button')
+  }
+
+function disableSaveButton(){
+  saveButton.disabled = true;
+}
+
+function clearInputs(){
+  titleInput.value = '';
+  bodyInput.value = '';
 }
